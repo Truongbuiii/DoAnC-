75b6880cf0395fd1849c0eb451919bbeb0d35b9f
PC <PC@LAPTOP-6F7G9G72>
2026-04-07 01:56:12 +0700

    upp

---
+++ SmartTourGuide.Web/AdminWeb/Controllers/ActivityLogsController.cs
@@ -0,0 +1,28 @@
+﻿using AdminWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminWeb.Controllers
+{
+    public class ActivityLogsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActivityLogsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Trang danh sách Log
+        public async Task<IActionResult> Index()
+        {
+            // Lấy dữ liệu kèm thông tin POI, sắp xếp cái mới nhất lên đầu
+            var logs = await _context.ActivityLogs
+                .Include(a => a.Poi)
+                .OrderByDescending(a => a.AccessTime)
+                .ToListAsync();
+
+            return View(logs);
+        }
+    }
+}
